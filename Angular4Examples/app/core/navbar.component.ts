e9b3d2bc4c3fd55b8f4b7f661d0bf4ebbcc8a3cf
Ruud Vriens <ruudvriens@hotmail.com>
2017-06-14 00:17:01 +0200

    Initial

--- Angular4Examples/app/core/navbar.component.ts
+++ Angular4Examples/app/core/navbar.component.ts
@@ -1,10 +1,20 @@
-﻿import { Component } from '@angular/core';
+﻿import {
+    Component,
+    Input
+} from '@angular/core';
+import { Subject } from 'rxjs'
 
-
+interface NavElement
+{
+    link: string;
+    name: string;
+}
 
 @Component({
     selector: 'nav-bar',
     templateUrl: './navbar.component.html'
 })
 export class NavBarComponent {
+    @Input() navElements: Subject<NavElement[]>;
+
 }
